fd8113a300e483b69636ae80ed237d8205c9671e
김재석 <60083524+iai6203@users.noreply.github.com>
2025-01-10 08:15:35 +0900

    fix(nvim): neo-tree '부모 노드로 이동'에 대한 잘못된 설정 수정

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,26 +3,16 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     cmd = "Neotree",
-    keys = {
-      {
-        "h",
-        function()
-          -- Neo-tree 버퍼인지 확인
-          if vim.bo.filetype == "neo-tree" then
-            local state = require("neo-tree.sources.manager").get_state("filesystem")
-            local node = state.tree:get_node()
-
-            if node.type == "directory" and node:is_expanded() then
-              require("neo-tree.sources.filesystem").toggle_directory(state, node)
-            else
+    opts = {
+      window = {
+        mappings = {
+          ["h"] = {
+            function(state)
+              local node = state.tree:get_node()
               require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-            end
-          else
-            -- 기본 Vim 동작 수행
-            vim.api.nvim_feedkeys("h", "n", false)
-          end
-        end,
-        desc = "Focus Parent Node in Neo-tree (if active), otherwise default 'h'",
+            end,
+          },
+        },
       },
     },
   },
